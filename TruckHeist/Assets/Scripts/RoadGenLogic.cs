ea842b6af8c0f92c6bb78c8787d2cc538f33ef50
Kyle Moore <56to74@gmail.com>
2020-06-02 23:56:55 -0400

    added endless road logic to Game Manger object.

---
+++ TruckHeist/Assets/Scripts/RoadGenLogic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadGenLogic : MonoBehaviour
+{
+    int nextStep = 60;
+    public Transform startPos;
+    [SerializeField]
+    GameObject road;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(road, new Vector3(startPos.position.x, startPos.position.y, startPos.position.z + nextStep), Quaternion.identity);
+        nextStep += 60;
+    }
+}
