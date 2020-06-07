bbdeaa9b3850a51cd3544f7efea92cea1f15689e
Kyle Moore <56to74@gmail.com>
2020-06-07 09:34:15 -0400

    added basic car movement and cimemachine camera

---
+++ TruckHeist/Assets/Scripts/CarController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider m_leftWheel;
+    public WheelCollider m_rightWheel;
+    public bool m_motor;
+    public bool m_steering;
+}
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> m_axleInfos;
+    public float m_maxTorqueSpeed = 150.0f;
+    public float m_maxSteeringAngle = 45f;
+
+
+    public void ApplyLocalPositionVisuals(WheelCollider collider) {
+        if(collider.transform.childCount == 0) {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float motor = m_maxTorqueSpeed * Input.GetAxis("Vertical");
+        float steering = m_maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in m_axleInfos) {
+            if (axleInfo.m_steering) {
+                axleInfo.m_leftWheel.steerAngle = steering;
+                axleInfo.m_rightWheel.steerAngle = steering;
+            }
+
+            if (axleInfo.m_motor) {
+                axleInfo.m_leftWheel.motorTorque = motor;
+                axleInfo.m_rightWheel.motorTorque = motor;
+            }
+
+            ApplyLocalPositionVisuals(axleInfo.m_leftWheel);
+            ApplyLocalPositionVisuals(axleInfo.m_rightWheel);
+        }
+    }
+}
---
+++ TruckHeist/Assets/Scripts/FreeLookLogic.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FreeLookLogic : MonoBehaviour
+{
+    public float m_screenoffset = 1f;
+    public float m_cameraSpeedX = 0f;
+    public float m_maxCameraSpeedX = 1.5f;
+    public float m_cameraSpeedY = 0f;
+    public float m_maxCameraSpeedY = 3f;
+
+    bool m_controllerDetected = false;
+
+    public CinemachineFreeLook cm_freeLook;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm_freeLook = GetComponent<CinemachineFreeLook>();
+        m_controllerDetected = DetectController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_controllerDetected = DetectController();
+        UpdateCameraValue(m_controllerDetected);
+        cm_freeLook.m_XAxis.m_InputAxisValue = m_cameraSpeedX;
+        cm_freeLook.m_YAxis.m_InputAxisValue = m_cameraSpeedY;
+    }
+
+    void UpdateCameraValue(bool controllerDetected) {
+        if(!controllerDetected) {
+            if (Input.mousePosition.x >= Screen.width - m_screenoffset) {
+                m_cameraSpeedX = m_maxCameraSpeedX;
+            } else if (Input.mousePosition.x <= m_screenoffset) {
+                m_cameraSpeedX = -m_maxCameraSpeedX;
+            } else {
+                m_cameraSpeedX = Input.GetAxis("Mouse X");
+            }
+
+            if (Input.mousePosition.y >= Screen.height - m_screenoffset) {
+                m_cameraSpeedY = m_maxCameraSpeedY;
+            } else if (Input.mousePosition.y <= m_screenoffset) {
+                m_cameraSpeedY = -m_maxCameraSpeedY;
+            } else {
+                m_cameraSpeedY = Input.GetAxis("Mouse Y") * 0.5f;
+            }
+        } else {
+            m_cameraSpeedX = Input.GetAxis("Mouse X");
+            m_cameraSpeedY = Input.GetAxis("Mouse Y") * 0.5f;
+        }
+    }
+
+    bool DetectController() {
+        //Get Joystick Names
+        string[] temp = Input.GetJoystickNames();
+        //Check whether array contains anything
+        if(temp.Length > 0) {
+            //Iterate over every element
+            for (int i = 0; i < temp.Length; i++) {
+                //Check if the string is empty of not
+                if (!string.IsNullOrEmpty(temp[i])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
